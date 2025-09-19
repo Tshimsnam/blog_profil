d1299803071c129ddd1f349c381b7b271f922c69
Tshims <manassetshims@gmail.com>
2025-09-19 01:15:29 +0100

    mise en place des migration et installation de breeze pour login et register

---
+++ app/Models/Profile.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Profile extends Model
+{
+     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'job',
+        'email',
+        'phone',
+        'bio',
+        'photo',
+        'qr_code',
+    ];
+}
